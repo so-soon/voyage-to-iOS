ad6d12956bd0d638d0e60a8fec2e8921ce576b04
randy <rs@gaudiolab.com>
2021-02-11 19:54:02 +0900

    add : load first page

--- Assignment/W01/RANDY/MyWebBrowser/MyWebBrowser/ViewController.swift
+++ Assignment/W01/RANDY/MyWebBrowser/MyWebBrowser/ViewController.swift
@@ -29,6 +29,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.webView.navigationDelegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let firstURL: URL?
+        
+        firstURL = URL(string: "https://www.apple.com")
+        
+        guard let url = firstURL else {
+            //invalid URL
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        self.webView.load(urlRequest)
+        
+        
+    }
+    
+    func setNetworkIndicatorVisible(_ isFinishedNavigator : Bool){
+        self.loadingIndicator.isHidden = isFinishedNavigator
+        isFinishedNavigator ? self.loadingIndicator.stopAnimating() : self.loadingIndicator.startAnimating()
+        // isNetworkActivityIndicatorVisible is Deprecated in iOS 13
+        if #available(iOS 13.0, *){}else{
+            UIApplication.shared.isNetworkActivityIndicatorVisible = isFinishedNavigator
+        }
     }
 }
 
@@ -36,14 +65,27 @@ class ViewController: UIViewController {
 
 extension ViewController: WKNavigationDelegate{
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.setNetworkIndicatorVisible(true)
         
+        webView.evaluateJavaScript("document.title"){(value: Any?, error: Error?) in
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            }
+            
+            guard let title = value as? String else{
+                return
+            }
+                
+            self.navigationItem.title = title
+        }
     }
     
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         
     }
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        
+        self.setNetworkIndicatorVisible(false)
     }
 }
 
