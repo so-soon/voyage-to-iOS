2d8307e50368ab7a76ce5fd638eb6a05aaaae462
randy <rs@gaudiolab.com>
2021-02-11 19:32:47 +0900

    add : Basic IBAction , IBOutlet, Delegate extension for webkit

--- Assignment/W01/RANDY/MyWebBrowser/MyWebBrowser/ViewController.swift
+++ Assignment/W01/RANDY/MyWebBrowser/MyWebBrowser/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var btnBack: UIBarButtonItem!
+    @IBOutlet weak var btnRefresh: UIBarButtonItem!
+    @IBOutlet weak var btnForward: UIBarButtonItem!
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    @IBAction func pressedBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func pressedRefresh(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func pressedForward(_ sender: UIBarButtonItem) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
 
+
+extension ViewController: WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        
+    }
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        
+    }
 }
 
