761da9544ff1085893326e26f639185ee370edb3
randy <rs@gaudiolab.com>
2021-02-11 18:37:34 +0900

    init : Init Project

---
+++ Assignment/W01/RANDY/MyWebBrowser/MyWebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWebBrowser
+//
+//  Created by Randy on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
