2edcf1315ef6770fa99f030ec4e6b5f540405585
daniel <sunghwan519@hotmail.com>
2021-02-20 22:16:56 +0900

    init: initial todo project

---
+++ Assignment/W02/Daniel/MyTodos/MyTodos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTodos
+//
+//  Created by 김성환 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
