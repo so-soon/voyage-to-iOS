549200a125acce863eca7793b5e10f729ba3f780
so-soon <hyuk0610@gmail.com>
2021-02-27 18:48:52 +0900

    add : basic MVVM files

---
+++ Assignment/W02/Randy/RxTodos/RxTodos/TodoTableView/TodoTableModel.swift
@@ -0,0 +1,12 @@
+//
+//  TodoTableModel.swift
+//  RxTodos
+//
+//  Created by Randy on 2021/02/27.
+//
+
+import Foundation
+
+class TodoTableModel {
+    
+}
---
+++ Assignment/W02/Randy/RxTodos/RxTodos/TodoTableView/TodoTableViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TodoTableViewModel.swift
+//  RxTodos
+//
+//  Created by Randy on 2021/02/27.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+class TodoTableViewModel {
+        
+}
