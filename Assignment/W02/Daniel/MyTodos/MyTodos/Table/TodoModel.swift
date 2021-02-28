9f684092f7375b358ad4be73a56090606d10ced9
daniel <sunghwan519@hotmail.com>
2021-02-28 20:58:51 +0900

    add: add basic model for todos

---
+++ Assignment/W02/Daniel/MyTodos/MyTodos/Table/TodoModel.swift
@@ -0,0 +1,36 @@
+//
+//  TodoModel.swift
+//  MyTodos
+//
+//  Created by 김성환 on 2021/02/28.
+//
+
+import Foundation
+
+class TodoModel {
+    private var todoItems = Array<TodoItem>()
+    
+    func addTodo(_ todoItem: TodoItem) {
+        
+    }
+    
+    func removeTodo(_ todoItem: TodoItem) {
+        
+    }
+    
+    func addNoti(_ todoItem: TodoItem) {
+        
+    }
+    
+    func removeNoti(_ todoItem: TodoItem) {
+        
+    }
+    
+    private func saveToJSON() {
+        
+    }
+    
+    private func loadFromJSON() {
+        
+    }
+}
