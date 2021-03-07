595acab7076340ecbf6783312e86c21534e6148a
daniel <sunghwan519@hotmail.com>
2021-03-07 20:48:04 +0900

    imp: add basic ui

---
+++ Assignment/W04/daniel/SimpleLoginView/SimpleLoginView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLoginView
+//
+//  Created by 김성환 on 2021/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
