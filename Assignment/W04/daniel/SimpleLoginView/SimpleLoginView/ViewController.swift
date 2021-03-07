a5c0411f38867bb1066be015556cfbec1c60b32a
daniel <sunghwan519@hotmail.com>
2021-03-07 21:27:21 +0900

    imp: implement timer

--- Assignment/W04/daniel/SimpleLoginView/SimpleLoginView/ViewController.swift
+++ Assignment/W04/daniel/SimpleLoginView/SimpleLoginView/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count:Int = 0
+    @IBOutlet weak var timerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.updTimer()
     }
-
-
+    
+    func updTimer() {
+        DispatchQueue.main.async {
+            self.timerLabel.text = String(self.count)
+        }
+        count += 1
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
+            self.updTimer()
+        }
+    }
 }
 
