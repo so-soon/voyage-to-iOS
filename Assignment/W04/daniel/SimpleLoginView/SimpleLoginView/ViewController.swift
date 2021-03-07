677fcbee1a593b1c286c707db26a1e5779b273f0
daniel <sunghwan519@hotmail.com>
2021-03-07 21:48:19 +0900

    imp: id pw check

--- Assignment/W04/daniel/SimpleLoginView/SimpleLoginView/ViewController.swift
+++ Assignment/W04/daniel/SimpleLoginView/SimpleLoginView/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     
     var count:Int = 0
+    var idPass:Bool = false
+    var pwPass:Bool = false
     @IBOutlet weak var timerLabel: UILabel!
-
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var pwField: UITextField!
+    @IBOutlet weak var idPassImage: UIImageView!
+    @IBOutlet weak var pwPassImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,6 +32,34 @@ class ViewController: UIViewController {
         count += 1
         DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) {
             self.updTimer()
+        }
+    }
+    @IBAction func idChangeEvent(_ sender: Any) {
+        guard let text = self.idField.text else {
+            return
+        }
+        if text.contains("@") &&
+            self.idField.text!.contains(".") {
+            idPassImage.image = UIImage(systemName: "checkmark.circle.fill")
+            self.idPass = true
+        }
+        else {
+            idPassImage.image = UIImage(systemName: "checkmark.circle")
+            self.idPass = false
+        }
+    }
+    @IBAction func pwChangeEvent(_ sender: Any) {
+        guard let text = self.pwField.text else {
+            return
+        }
+        
+        if text.count >= 5 {
+            pwPassImage.image = UIImage(systemName: "checkmark.circle.fill")
+            self.pwPass = true
+        }
+        else {
+            pwPassImage.image = UIImage(systemName: "checkmark.circle")
+            self.pwPass = false
         }
     }
 }
