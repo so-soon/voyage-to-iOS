d659a736e0816e98fc7c7ea6eceef758b3983b7d
alan <alan.jiwon.oh@gmail.com>
2021-03-09 14:16:19 +0900

    impl: counter based on dispatch queue - global timer, main updater

--- Assignment/W04/Alan/Login/Login/ViewController.swift
+++ Assignment/W04/Alan/Login/Login/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count: Int = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //setup counter
+        self.count = 0
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+            DispatchQueue.global().async {
+                self.count+=1
+                DispatchQueue.main.async {
+                    self.labelCount.text=String(self.count)
+                }
+            }
+        })
     }
 
-
+    @IBOutlet weak var labelCount: UILabel!
+    
 }
 
