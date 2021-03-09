9184430ce36a8cbb859bb7bdd091af74902c3bf1
alan <alan.jiwon.oh@gmail.com>
2021-03-09 15:49:57 +0900

    impl: update download image asynchronous

--- Assignment/W04/Alan/Login/Login/ViewController.swift
+++ Assignment/W04/Alan/Login/Login/ViewController.swift
@@ -14,19 +14,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        activityIndicator.hidesWhenStopped = true
+        
         //setup counter
         self.count = 0
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: { timer in
             DispatchQueue.global().async {
-                self.count+=1
+                self.count += 1
                 DispatchQueue.main.async {
-                    self.labelCount.text=String(self.count)
+                    self.labelCount.text = String(self.count)
                 }
             }
         })
     }
 
     @IBOutlet weak var labelCount: UILabel!
-    
+    @IBOutlet weak var downloadImage: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    @IBAction func login(_ sender: Any) {
+        guard
+            let url = URL(string: "https://picsum.photos/1280/720/?random")
+        else { return }
+       
+        DispatchQueue.main.async {
+            self.activityIndicator.startAnimating()
+        }
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self.downloadImage.image = image
+                        self.activityIndicator.stopAnimating()
+                    }
+                }
+            }
+        }
+    }
 }
 
